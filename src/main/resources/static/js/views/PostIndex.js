9d7df3d50d92d0958371c0794865b137253e34a8
dbc-hou <david.collins.hou@gmail.com>
2022-04-14 16:57:20 -0500

    further CRUD work on blog API

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -1,7 +1,8 @@
 import createView from "../createView.js";
 import fetchData from "../fetchData.js";
 
-var myMethod = "POST"
+var myMethod = "POST";
+var currentPostId = 1;
 
 export default function PostIndex(props) {
     return `
@@ -26,8 +27,8 @@ export default function PostIndex(props) {
                         <textarea class="form-control" id="add-post-content" row="5" value=""></textarea>
                     </div>
                     <div class="row">
-                        <button class="btn btn-primary" id="add-post-btn">Add Post</button>
-                        <button class="btn btn-primary" id="clear-btn">Clear</button>
+                        <button class="btn btn-primary col-2" id="add-post-btn">Add Post</button>
+                        <button class="btn btn-primary col-2" id="clear-btn">Clear</button>
                     </div>
                 </form>
             </div>
@@ -38,20 +39,25 @@ export default function PostIndex(props) {
 export function PostsEvent() {
     attachAddListener();
     attachEditListener();
-    //function 3: add listener to delete-post-btn
+    attachClearListener();
+    attachDeleteListener();
 }
 function attachAddListener () {
     $("#add-post-btn").click(function (e) {
-        myMethod = "POST";
-        const myTitle = $("#add-post-title").val();
-        const myContent = $("#add-post-content").val();
-        const myPost = {};
-        myPost.title = myTitle;
-        myPost.content = myContent;
-        const myRequest = {};
-        myRequest.method = myMethod;
-        myRequest.headers = {'Content-Type': 'application/json'};
-        myRequest.body = JSON.stringify(myPost);
+        if (myMethod == "POST") {
+            const myTitle = $("#add-post-title").val();
+            const myContent = $("#add-post-content").val();
+            const myPost = {};
+            myPost.title = myTitle;
+            myPost.content = myContent;
+            const myRequest = {};
+            myRequest.method = myMethod;
+            myRequest.headers = {'Content-Type': 'application/json'};
+            myRequest.body = JSON.stringify(myPost);
+            } else {
+                $("#title-" + currentPostId).text($("#add-post-title").val());
+                $("#content-" + currentPostId).text($("#add-post-content").val());
+            }
         fetch("http://localhost:8081/api/posts", myRequest)
             .then(res => {
                 console.log(res.status);
@@ -65,13 +71,28 @@ function attachAddListener () {
 
 function attachEditListener() {
     $(".edit-link").click(function () {
-        myMethod = "PUT"
-        $("add-post-btn").val("Update")
+        myMethod = "PUT";
+        $("#add-post-btn").text("Update");
         const postId = $(this).data("id");
         const postTitle = $("#title-" + postId).text();
         const postContent = $("#content-" + postId).text();
+        currentPostId = postId;
         console.log(postId);
         $("#add-post-title").val(postTitle);
         $("#add-post-content").val(postContent);
+    })
+}
+
+function attachDeleteListener() {
+    $(".delete-link").click(function () {
+        myMethod = "DELETE";
+    })
+}
+function attachClearListener() {
+    $("#clear-btn").click (function () {
+        myMethod = "POST";
+        $("#add-post-btn").text("Add Post");
+        $("#add-post-title").val("");
+        $("#add-post-content").val("");
     })
 }
