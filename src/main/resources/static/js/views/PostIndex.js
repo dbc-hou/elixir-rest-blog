02304f6a396cae82e35cd79d3993cee45583b068
dbc-hou <david.collins.hou@gmail.com>
2022-04-18 09:51:59 -0500

    delete functionality added

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -4,19 +4,26 @@ import fetchData from "../fetchData.js";
 var myMethod = "POST";
 var currentPostId = 1;
 
+//All this does is return a long, complicated string of html, css, & object properties expressed in jquery.
 export default function PostIndex(props) {
     return `
+
+<!--The header section is very simple.-->
         <header>
-            <h1>Posts Page</h1>
+            <h1 class="container-fluid fw-bolder text-success">Posts Page</h1>
         </header>
+
+<!--The main section contains the list of posts and the add/edit form.-->
         <main>
-            <div id="posts-container">
-                ${props.posts.map(post => `<h3 id="title-${post.id}">${post.title}</h3>
+<!--Here is the list of posts. The map function substitutes for a for-each loop.-->
+            <div id="posts-container" class="container-fluid">
+                ${props.posts.map(post => `<h3 class="fw-bold" id="title-${post.id}">${post.title}</h3>
                     <h4 id="content-${post.id}">${post.content}</h4>
                     <a href="#" class="edit-link" data-id="${post.id}">Edit</a>
                     <a href="#" class="delete-link" data-id="${post.id}">Delete</a></p>`).join('')}   
             </div>
-            <div class="container" id="add-post">
+<!--Here is the add/edit form.-->
+            <div class="container-fluid" id="add-post">
                 <form id="add-post-form">
                     <div class="mb-3">
                         <label for="add-post-title">Title</label>
@@ -26,7 +33,7 @@ export default function PostIndex(props) {
                         <label for="add-post-content">Content</label>
                         <textarea class="form-control" id="add-post-content" row="5" value=""></textarea>
                     </div>
-                    <div class="row">
+                    <div class="mb-4">
                         <button class="btn btn-primary col-2" id="add-post-btn">Add Post</button>
                         <button class="btn btn-primary col-2" id="clear-btn">Clear</button>
                     </div>
@@ -86,6 +93,18 @@ function attachEditListener() {
 function attachDeleteListener() {
     $(".delete-link").click(function () {
         myMethod = "DELETE";
+        const postId = $(this).data("id");
+        const myRequest = {};
+        myRequest.method = myMethod;
+        myRequest.headers = {'Content-Type': 'application/json'};
+        fetch("http://localhost:8081/api/posts/" + postId, myRequest)
+            .then(res => {
+                console.log(res.status);
+                createView("/posts")
+            }).catch(error => {
+            console.log(error);
+            createView("/posts");
+        });
     })
 }
 function attachClearListener() {
